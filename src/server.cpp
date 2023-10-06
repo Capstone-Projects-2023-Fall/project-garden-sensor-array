9f1dbe2c1a82a8143e5a20952dedb37e384370f4
Sam Gandolfo-Lucia <tuj91536@temple.edu>
2023-10-06 11:53:03 -0400

    Added characterisics

--- src/server.cpp
+++ src/server.cpp
@@ -8,14 +8,14 @@
 #include "Adafruit_seesaw.h"
 #include "hp_BH1750.h"
 
-#define UUIDSERVER  "efab93a7-3e27-4c38-a9cc-f768cfc75af6"  // not sure this is needed 
-#define UUIDSERVICE "2aba97e0-10b4-4a9f-bb43-35e02e6eb594"
-#define UUIDCHAR1   "f524c885-8158-49b7-9ff6-0d653c3cafb5"
-#define UUIDCHAR2   "0308e66e-8096-4b60-b077-892bc738e8f1"
-#define UUIDCHAR3   "497b9303-3658-45f7-85ac-e1caadb066fa"
+#define UUIDSERVER      "efab93a7-3e27-4c38-a9cc-f768cfc75af6"  // not sure this is needed 
+#define UUIDSERVICE     "2aba97e0-10b4-4a9f-bb43-35e02e6eb594"
+#define UUIDCHAR1       "f524c885-8158-49b7-9ff6-0d653c3cafb5"
+#define UUIDCHAR2       "0308e66e-8096-4b60-b077-892bc738e8f1"
+#define UUIDCHAR3       "497b9303-3658-45f7-85ac-e1caadb066fa"
 
-Adafruit_seesaw soil_sensor;
-hp_BH1750 light_sensor;
+Adafruit_seesaw     soil_sensor;
+hp_BH1750           light_sensor;
 
 void setup() {
     Serial.begin(115200);
@@ -29,13 +29,25 @@ void setup() {
     
     // characteristics
     BLECharacteristic *soil_moisture_char = service->createCharacteristic(UUIDCHAR1, ESP_GATT_CHAR_PROP_BIT_READ | ESP_GATT_CHAR_PROP_BIT_NOTIFY);
-    BLECharacteristic *soil_temp_char = service->createCharacteristic(UUIDCHAR1, ESP_GATT_CHAR_PROP_BIT_READ | ESP_GATT_CHAR_PROP_BIT_NOTIFY);
-    BLECharacteristic *lux_char = service->createCharacteristic(UUIDCHAR1, ESP_GATT_CHAR_PROP_BIT_READ | ESP_GATT_CHAR_PROP_BIT_NOTIFY);
-    
-    // try initializing both sensors. if either fails, hault the program.
+    BLECharacteristic *soil_temp_char = service->createCharacteristic(UUIDCHAR2, ESP_GATT_CHAR_PROP_BIT_READ | ESP_GATT_CHAR_PROP_BIT_NOTIFY);
+    BLECharacteristic *lux_char = service->createCharacteristic(UUIDCHAR3, ESP_GATT_CHAR_PROP_BIT_READ | ESP_GATT_CHAR_PROP_BIT_NOTIFY);
+
+    // 
+    soil_moisture_char->setValue(0);
+    soil_temp_char->setValue(0);
+    lux_char->setValue(0);
+
+    // here we go
+    service->start();
+
+    // start advertising
+    BLEAdvertising *advertisement = server->getAdvertising();
+    advertisement->start();
+
+    // looks like bluetooth is set up. lets try to start the sensors
     if( !soil_sensor.begin(0x36) || !light_sensor.begin(BH1750_TO_GROUND) ) {
         Serial.println("Sensor error!");
-        while(1); // infinite loop
+        while(1); // hault program, something's not right
     }
 
     Serial.println("Sensors initialized!");
