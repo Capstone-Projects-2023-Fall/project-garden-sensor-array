c5142773b6c30079298c4efe130e04aed01e69f0
tuj91536 <tuj91536@temple.edu>
2023-11-07 23:06:54 -0500

    predefine sleep interval with constant

--- src/HUB_code/client.py
+++ src/HUB_code/client.py
@@ -12,19 +12,18 @@ import time
 
 from bleak import BleakScanner
 
+SLEEP_INTERVAL_S = 1
 
 async def main():
     async with BleakScanner() as scanner:
-        print("Scanning...")
-
-        print(f"\nadvertisement packets:")
-        n = 5
+        
+        n = 5 # number of packets to reach each sleep interval
         async for bd, ad in scanner.advertisement_data():
             print(f"{bd!r} with {ad!r}")
             n -= 1
             if n == 0:
                 n = 5
-                time.sleep(1)
+                time.sleep(SLEEP_INTERVAL_S)
             
 
 if __name__ == "__main__":
