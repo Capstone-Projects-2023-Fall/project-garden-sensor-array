531a2c2e2b91e5014afa4d62ecac493501da01ca
Sam Gandolfo-Lucia <tuj91536@temple.edu>
2023-11-29 22:55:31 -0500

    sets MT register of bh1750 sensor

--- src/SCU_Code/bh1750/bh1750.c
+++ src/SCU_Code/bh1750/bh1750.c
@@ -24,9 +24,31 @@ bool bh1750_makeamove(uint8_t mode) {
     bh1750_setMTreg(BH1750_DEFAULT_MTREG); // theoretically this could fail but i dont want to check that 
     return true; // guess we're good then!
 }
-
+ 
+// short for MTregister?
 void bh1750_setMTreg(uint8_t MTreg) {
+    if(MTreg < BH1750_MTREG_MIN || MTreg > BH1750_MTREG_MAX) {
+        puts("bad value!");
+        return;
+    }
 
+    uint8_t ack = 0; // acknowledgement byte used for error checking. i didn't use it though!
+
+    // the following set of bit shifts and writes are just bh1750 hardware semantics
+    // basically its specsheet stuff that i took from the reference library
+    // obtains a byte by doing a logical or on the MTreg value and a bit string
+    // then write it to the bh1750
+    uint8_t value = (0b01000 << 3) | (MTreg >> 5);
+    i2c_write_blocking(I2C_PORT, BH1750_ADDR, &value, 1, false);
+    
+    value = (0b011 << 5) | (MTreg & 0b11111);
+    i2c_write_blocking(I2C_PORT, BH1750_ADDR, &value, 1, false);
+
+    i2c_write_blocking(I2C_PORT, BH1750_ADDR, &BH1750_MODE, 1, false);
+
+    sleep_ms(10); // boring!
+    
+    BH1750_MTREG = MTreg; // need to keep track of this
 }
 
 float bh1750_read_lux() {
