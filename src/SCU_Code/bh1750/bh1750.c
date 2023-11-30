c337c85494251a3c0ed79be51ea68dfa70e6c26d
Sam Gandolfo-Lucia <tuj91536@temple.edu>
2023-11-29 23:07:38 -0500

    bh1750 library tested and working

--- src/SCU_Code/bh1750/bh1750.c
+++ src/SCU_Code/bh1750/bh1750.c
@@ -77,21 +77,3 @@ float bh1750_read_lux() {
     level /= BH1750_CONV_FACTOR; // simple conversion
     return level; // there it is
 }
-
-int main()
-{
-    stdio_init_all();
-
-    // I2C Initialisation. Using it at 400Khz.
-    i2c_init(I2C_PORT, 100*1000);
-    
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA);
-    gpio_pull_up(I2C_SCL);
-
-
-    puts("Hello, world!");
-
-    return 0;
-}
