e83a12be79d6fb477a150745ed6b0696133399a0
Gabriel Sta Ana <gistaana8755@gmail.com>
2023-12-03 23:20:04 -0500

    idek

--- gsa-website/src/components/CardData.js
+++ gsa-website/src/components/CardData.js
@@ -5,7 +5,7 @@ import { useState, useRef, useEffect} from "react";
 
 import { auth } from '../firebase';
 import Authenticate from './Authenticate';
-import { getDatabase, ref, onValue } from "firebase/database";
+import { getDatabase, ref, onValue, get } from "firebase/database";
 //Temperature
 import AcUnitTwoToneIcon from '@mui/icons-material/AcUnitTwoTone'; //Low Temp - snowflake 
 import WbSunnyTwoToneIcon from '@mui/icons-material/WbSunnyTwoTone'; // Good Temp - sun 
@@ -30,6 +30,7 @@ const CardData = (props) => {
     const [openSunlight, setOpenSunlight] = useState(false);
 
     const db = getFirestore();
+    const fs = getDatabase();
 
     const currentDate = new Date();
     let currDate = new Date();
@@ -48,75 +49,82 @@ const CardData = (props) => {
     console.log(currentHub)
 
     const { authUser } = Authenticate();
-    var q
-     
-    if (authUser) {
-        // Get the user's UID from the authUser object
-        const userUid = authUser.uid;
-    
-
-        const fs = getDatabase();
-        
-        const dbref = ref(fs, 'Users/' + userUid + '/' + currentHub);
-        var serial
-        onValue(dbref, (snapshot) => {
-            console.log("Check=====" + currentDate)
-            serial = snapshot.val().Serial
-            console.log("|" + serial + "|")
-        });
-            
-        q = query(collection(db, "DATA"), 
-                orderBy("Time", "asc"), 
-                where("Time", "<", currDate),
-                where("Time", ">", prevDate));
-                //where("AccountID", "==", serial)); this breaks everything for reasons beyond my comprehension
-    }
 
     const [sun, setSun] = useState([]);
     const [moi, setMoi] = useState([]);
     const [tem, setTem] = useState([]);
     const [dates, setDates] = useState([]);
+    const [serial, setSerial] = useState('');
 
+    
     const [avg, setAvg] = useState({ "Temperature": 0, "Moisture": 0, "Sunlight": 0 });
 
     async function fillCard() {
-        try {
-          const querySnapshot = await getDocs(q);
-          querySnapshot.forEach((doc) => {
-            console.log("|" + doc.data().Temperature + "|")
-            setAvg(prevAvg => ({              // total of all stats
-                ...prevAvg,
-                Temperature: prevAvg.Temperature + doc.data().Temperature,
-                Moisture: prevAvg.Moisture + doc.data().Moisture,
-                Sunlight: prevAvg.Sunlight + doc.data().Sunlight
-              }));
-    
-            setSun((prevSun) => [...prevSun, doc.data().Sunlight]);   // adding data 
-            setMoi((prevMoi) => [...prevMoi, doc.data().Moisture]);
-            setTem((prevTem) => [...prevTem, doc.data().Temperature]);
-    
-            var date = doc.data().Time.toDate();
-            setDates((prevDates) => [
-              ...prevDates,
-              date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds(),
-            ]);
-          });
-          
-          setAvg(prevAvg => ({   // dividing averages via querySnapshot size instead of a count var
-            ...prevAvg,
-            Temperature: prevAvg.Temperature / querySnapshot.size,
-            Moisture: prevAvg.Moisture / querySnapshot.size,
-            Sunlight: prevAvg.Sunlight / querySnapshot.size
-          }));  
-        } catch (error) {
-          console.log("Error fetching data:", error);
-        }
-      }
-    
-      useEffect(() => { // useEffect ensures it only runs when mounted
-        fillCard();
-      }, []);
+      var s
+      var q
+      var qry
 
+      try {              
+
+        if (authUser) {
+          // Get the user's UID from the authUser object
+          const userUid = authUser.uid  
+        
+        const dbref = ref(fs, 'Users/' + userUid + '/' + currentHub);
+        onValue(dbref, (snapshot) => {
+            console.log(snapshot.val().Serial)  // works
+            console.log("Check=====" + currentDate)
+            s = snapshot.val().Serial
+            setSerial(s); // doesnt fucking work
+            console.log("|" + s + "|") //fucking works
+
+            qry = query(collection(db, "DATA"), 
+                where("AccountID", "==", s));
+        });
+      }
+
+      q = query(collection(db, "DATA"), 
+                orderBy("Time", "asc"), 
+                where("Time", "<", currDate),
+                where("Time", ">", prevDate));
+
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+          console.log("|" + doc.data().Temperature + "|")
+          setAvg(prevAvg => ({              // total of all stats
+              ...prevAvg,
+              Temperature: prevAvg.Temperature + doc.data().Temperature,
+              Moisture: prevAvg.Moisture + doc.data().Moisture,
+              Sunlight: prevAvg.Sunlight + doc.data().Sunlight
+            }));
+          
+  
+          setSun((prevSun) => [...prevSun, doc.data().Sunlight]);   // adding data 
+          setMoi((prevMoi) => [...prevMoi, doc.data().Moisture]);
+          setTem((prevTem) => [...prevTem, doc.data().Temperature]);
+  
+          var date = doc.data().Time.toDate();
+          setDates((prevDates) => [
+            ...prevDates,
+            date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds(),
+          ]);
+        });
+        
+        setAvg(prevAvg => ({   // dividing averages via querySnapshot size instead of a count var
+          ...prevAvg,
+          Temperature: prevAvg.Temperature / querySnapshot.size,
+          Moisture: prevAvg.Moisture / querySnapshot.size,
+          Sunlight: prevAvg.Sunlight / querySnapshot.size
+        }));  
+      } catch (error) {
+        console.log("Error fetching data:", error);
+      }
+    }
+  
+    useEffect(() => { // useEffect ensures it only runs when mounted
+      fillCard();
+    },[ authUser]);
+    
   return (
     <>
     <Box>
