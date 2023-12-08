123fdfd0760b2153b11a5eabc0ef3a2fb9a868a1
Regina Oda <tuj58359@temple.edu>
2023-12-08 11:17:55 -0500

    Added space for Buttons

--- gsa-website/src/components/Pages/HubPage.js
+++ gsa-website/src/components/Pages/HubPage.js
@@ -20,12 +20,20 @@ const HubPage = () => {
 
   return (
     <>
-      <Layout />   
+      <Layout />    
       <Box m="20px">
-      {/* GRID & CHARTS */}
-      <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gridAutoRows="240px" gap="20px">
+      {/* Below is the Grid of the General HubPage */}
+      <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gridAutoRows="240px" gap="20px">  
 
-        {/* Row 1 */}
+
+        {/* Row 1: Header */}
+        <Box sx={{ border: 1 }}  gridColumn="span 12"  p="30px">
+            <Typography variant="h5" fontWeight="600">
+              {currentHub}'s Hubpage .. Welcome to your ... hubpage! Here you can find ....
+            </Typography>
+          </Box>  
+
+        {/* Row 2 - Hub Statistics  */}
         <Box sx={{ border: 1 }}  gridColumn="span 3" gridRow="span 2"   p="30px">
           <Typography variant="h5" fontWeight="600">
             What the Data Means 
@@ -35,9 +43,9 @@ const HubPage = () => {
           </Box>
         </Box>  
 
-        <Box sx={{ border: 1 }} gridColumn="span 6" gridRow="span 2"   p="30px">
+        <Box sx={{ border: 3 }} gridColumn="span 6" gridRow="span 2"   p="30px">
           <Typography variant="h5" fontWeight="600">
-            {currentHub} Info 
+            {currentHub}  
           </Typography>
           <Box height="250px" m="-20px 0 0 0" display="flex" flexDirection="column" alignItems="center" mt="25px"> 
             Stats 
@@ -52,30 +60,52 @@ const HubPage = () => {
           <Box  height="250px" m="-20px 0 0 0"display="flex" flexDirection="column" alignItems="center" mt="25px"> 
             Component similar to IconBox, alerts the user on what they should focus on -
           </Box>
-        </Box> 
+        </Box>   
 
-  
 
-        {/* Row 2 */} 
-        <Box  sx={{ border: 1 }} gridColumn="span 6" gridRow="span 2"  >
+          {/* Row 3: Statistic Buttons */}
+          <Box sx={{ border: 1 }}  gridColumn="span 12"  p="30px">
+              <Typography variant="h5" fontWeight="600">
+                  Weekday Buttons and Update Hub Button 
+              </Typography>
+          </Box>  
+
+
+
+
+        {/* Row 4 - Line Graphs */} 
+        <Box  sx={{ border: 1 }} gridColumn="span 4" gridRow="span 2"  >
           <Box mt="25px" p="0 30px" display="flex " justifyContent="space-between" alignItems="center">
             <Box>
               <Typography variant="h5" fontWeight="600" >
-                Line Graph
+                Moisture Line Chart 
               </Typography>
             </Box>
           </Box>
-          <Box height="250px" m="-20px 0 0 0">
-            {/* <LineChart isDashboard={true} /> */}
+          <Box height="250px" m="-20px 0 0 0"> 
+          {/* Space for Another Graph or figure - prob Bar Graph  */}
           </Box>
         </Box>  
 
 
-        <Box  sx={{ border: 1 }}gridColumn="span 6" gridRow="span 2"  >
+        <Box  sx={{ border: 1 }}gridColumn="span 4" gridRow="span 2">
           <Box mt="25px" p="0 30px" display="flex " justifyContent="space-between" alignItems="center">
             <Box>
               <Typography variant="h5" fontWeight="600"   >
-                Space for other Graph or figure
+                Temperature Line Chart
+              </Typography>
+            </Box>
+          </Box>
+          <Box height="250px" m="-20px 0 0 0">
+            {/* Space for Another Graph or figure - prob Bar Graph  */}
+          </Box>
+        </Box>  
+
+        <Box  sx={{ border: 1 }}gridColumn="span 4" gridRow="span 2">
+          <Box mt="25px" p="0 30px" display="flex " justifyContent="space-between" alignItems="center">
+            <Box>
+              <Typography variant="h5" fontWeight="600"   >
+                Sunight Line Chart
               </Typography>
             </Box>
           </Box>
@@ -86,7 +116,7 @@ const HubPage = () => {
 
 
 
-        {/* Row 3 */}
+        {/* Row 5 - Access More Data */}
         <Box gridColumn="span 4" gridRow="span 2"   p="30px">
           <Typography variant="h5" fontWeight="600">
             Free Space 
@@ -98,10 +128,20 @@ const HubPage = () => {
 
         <Box sx={{ border: 1 }} gridColumn="span 4" gridRow="span 2"   p="30px">
           <Typography variant="h5" fontWeight="600">
-            Access Hub's Recorded Data
+            Access More of Your Hub's Data: 
           </Typography>
-          <Box display="flex" flexDirection="column" alignItems="center" mt="25px"> 
-            Button 
+          <Box display="flex" flexDirection="column" alignItems="center" mt="25px">  
+            <Box>
+              <Typography  >
+                Go to {currentHub}'s SCU page to find the readings of each individual sensor. 
+              </Typography> 
+                Button 
+                <Box>
+              <Typography>
+                Click on "Show History Button to see your Hub's Recorded Averages."
+              </Typography>
+            </Box>
+            </Box>   
           </Box>
         </Box>  
 
@@ -123,6 +163,8 @@ const HubPage = () => {
 };
 
 export default HubPage; 
+
+
 
 
 
