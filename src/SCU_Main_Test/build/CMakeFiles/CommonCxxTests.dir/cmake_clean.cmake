f4c934cb358c535c209c9a2fc1ff64e382d14af1
ak74ub <76227413+ak74ub@users.noreply.github.com>
2023-12-08 21:03:22 +0000

    Addbuild

---
+++ src/SCU_Main_Test/build/CMakeFiles/CommonCxxTests.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CommonCxxTests.dir/CommonCxxTests.cxx.obj"
+  "CMakeFiles/CommonCxxTests.dir/CommonCxxTests.cxx.obj.d"
+  "CMakeFiles/CommonCxxTests.dir/testformat_sensor_data.cpp.obj"
+  "CMakeFiles/CommonCxxTests.dir/testformat_sensor_data.cpp.obj.d"
+  "CMakeFiles/CommonCxxTests.dir/testgetASSMsoilmoisture.cpp.obj"
+  "CMakeFiles/CommonCxxTests.dir/testgetASSMsoilmoisture.cpp.obj.d"
+  "CMakeFiles/CommonCxxTests.dir/testgetASSMtempC.cpp.obj"
+  "CMakeFiles/CommonCxxTests.dir/testgetASSMtempC.cpp.obj.d"
+  "CMakeFiles/CommonCxxTests.dir/testgetBH1750lux.cpp.obj"
+  "CMakeFiles/CommonCxxTests.dir/testgetBH1750lux.cpp.obj.d"
+  "CMakeFiles/CommonCxxTests.dir/testgetSensorData.cpp.obj"
+  "CMakeFiles/CommonCxxTests.dir/testgetSensorData.cpp.obj.d"
+  "CommonCxxTests.exe"
+  "CommonCxxTests.exe.manifest"
+  "CommonCxxTests.pdb"
+  "libCommonCxxTests.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CommonCxxTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/SCU_Main_Test/build/CMakeFiles/CommonCxxTests.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for CommonCxxTests.
+# This may be replaced when dependencies are built.
